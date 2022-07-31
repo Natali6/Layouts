582eb0d72db129b846ceec7374da33aac135368c
Natalia <holovkonatalia497@gmail.com>
2022-07-31 17:16:23 +0300

    add header top

--- Artificial Intelligence/js/script.js
+++ Artificial Intelligence/js/script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const menuIcon = document.getElementById("menu-icon");
+
+if (menuIcon) {
+  const menuList = document.getElementById("header__menu-list");
+  menuIcon.addEventListener("click", function () {
+    menuIcon.classList.toggle("active");
+    menuList.classList.toggle("active");
+  });
+}
